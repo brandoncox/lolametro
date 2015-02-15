cf2dd499550c8bb6588dc1f56088ae390c116ca5
Brandon Cox <bcox@redhat.com>
2015-02-14 21:38:33 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lolametro::Application.config.secret_key_base = 'b34c33b732f57c1ffdc5c32b6afc52f079944c9428786cca38c679fee7154733d637980283cec11a2460ed9552a063d48b1db1f30b7ddbf44011893755f3b58a'
